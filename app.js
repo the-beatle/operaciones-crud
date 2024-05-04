d82d08879bfa272a7cec1673c196499d0869a53e
the-beatle <marcae7@gmail.com>
2024-05-04 16:37:33 -0500

    destroy operation added

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 
-const pets = [];
+let pets = [];
 const form = document.getElementById("pet-form");
+
+function findId(event) {
+    return event.target.parentElement.parentElement.id;
+}
+
+function deletePet(event) {
+    event.target.parentElement.parentElement.remove()
+    const id = findId(event);
+    pets = pets.filter(pet => pet.id !== parseInt(id));
+    console.log(pets)
+}
 
 function createPetStringTemplate(pet) {
     return `
-           <div class="card" style="width: 18rem;">
+           <div id='${pet.id}' class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">Nombre: ${pet.name}</h5>
                 <p class="card-text">Propietario:${pet.owner}</p>
-                <a href="#" class="btn btn-primary">Actualizar</a>
-                <a href="#" class="btn btn-primary">Borrar</a>
+                <button class="btn btn-primary">Actualizar</button>
+                <button onclick="deletePet(event)"  class="btn btn-primary">Borrar</button>
             </div>
         </div> 
     `
@@ -19,6 +30,7 @@ form.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(form);
     const pet = {
+        id: pets.length+1,
         name: formData.get("name"),
         owner: formData.get("owner"),
     }
