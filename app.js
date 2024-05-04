642f47f99c1b46e26fe56664512b90569f310756
Mario Andrés Cano Echavarría <marcae7@gmail.com>
2024-05-04 16:02:00 -0500

    create form

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-const h1 = document.getElementsByTagName("h1")
-console.log(h1)
+
+const pets = [];
+const form = document.getElementById("pet-form");
+
+function createPetStringTemplate(pet) {
+    return `
+           <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Nombre: ${pet.name}</h5>
+                <p class="card-text">Propietario:${pet.owner}</p>
+                <a href="#" class="btn btn-primary">Actualizar</a>
+                <a href="#" class="btn btn-primary">Borrar</a>
+            </div>
+        </div> 
+    `
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(form);
+    const pet = {
+        name: formData.get("name"),
+        owner: formData.get("owner"),
+    }
+    pets.push(pet)
+    const petList = document.getElementById("pet-list");
+    petList.innerHTML+= createPetStringTemplate(pet);
+
+    form.reset();
+});
